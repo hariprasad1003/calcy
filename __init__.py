800fa25479bd42fc6616158a8a470918dbd395d9
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-03-12 23:23:03 +0530

    removed calc

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
-from calcy-math import *
+def add(self, a, b):
+    return a+b
+
+def sub(self, a, b):
+    return a-b
+
+def mul(self, a, b):
+    return a*b
+
+def div(self, a, b):
+    return a/b
--- calc.py
+++
@@ -1,8 +0,0 @@
-def add(self, a, b):
-    return a+b
-def sub(self, a, b):
-    return a-b
-def mul(self, a, b):
-    return a*b
-def div(self, a, b):
-    return a/b
