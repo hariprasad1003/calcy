f14fe18a6f1c337c4bf486550b416550d6a78f79
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-03-12 22:40:27 +0530

    base commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# calcy
+## _A Simple Math Library_
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+## Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+cd my_app
+pip install calcy
+```
+
+Importing calcy
+
+```
+from calcy import *
+```
+Running Server
+```
+python app.py
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    description = file.read()
+
+setuptools.setup(
+    name="calcy",
+    packages=setuptools.find_packages(),
+    version="0.0.1",
+    license='MIT', 
+    author="Hari Prasad",
+    author_email="hariprasadgurunathan10@gmail.com",
+    description="A simple math library",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hariprasad1003/calcy",
+    download_url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',   
+    install_requires=[]
+)
